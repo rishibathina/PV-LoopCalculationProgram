d457df7567e2f93f087a6666e883327d79df1bb4
Rek505 <57535849+Rek505@users.noreply.github.com>
2022-02-05 17:08:56 -0800

    commented out stuff

--- main.py
+++ main.py
@@ -32,18 +32,18 @@ def graph(xValues, yValues):
 # Residual volume is for Flow V Volume
 # pre: a NumPy array (x axis) of Volume(t)
 #      a NumPy array (y-axis) of Flow
-def residualVolume(volume, flow):
-    for (x, y) in zip(volume, flow):
-        if x != 0 and y == 0 :
-            return x
-
-    return "no data found"
-
-# Total Lung Capacity
-# pre: a numpy array (x-axis) of Time
-#      a numpy array (y-axis) of volume
-def totalLungCapacity(volume):
-    return max(volume)
+# def residualVolume(volume, flow):
+#     for (x, y) in zip(volume, flow):
+#         if x != 0 and y == 0 :
+#             return x
+#
+#     return "no data found"
+#
+# # Total Lung Capacity
+# # pre: a numpy array (x-axis) of Time
+# #      a numpy array (y-axis) of volume
+# def totalLungCapacity(volume):
+#     return max(volume)
 
 # pressure volume code
 
@@ -52,8 +52,8 @@ def totalLungCapacity(volume):
 volume = np.array([0,1,2,3,4,5])
 flow = np.array([0,1,3,5,6,0])
 
-cock = residualVolume(volume, flow)
-print(cock)
+# cock = residualVolume(volume, flow)
+# print(cock)
 
 #given volume(t), pressure(t), and flow(t)
 # need to create flow(volume)
