e6dbe42b5c1cc29ac62bbfd1edc382c1413bcc2b
Rek505 <57535849+Rek505@users.noreply.github.com>
2022-02-05 16:25:30 -0800

    residual volume funciton completed

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import json
 import numpy as np
 
-xpoints = np.array([0,5])
-ypoints = np.array([0,250])
+# xpoints = np.array([0,5])
+# ypoints = np.array([0,250])
+#
+# plt.plot(xpoints, ypoints)
+# plt.show()
 
-plt.plot(xpoints, ypoints)
-plt.show()
+ # open json file
+ # pre: file name (in json)
+def loadJson(jsonFile):
+    with open(jsonFile, 'r') as fileData:
+        data = fileData.read()
 
-# pressure
-# flow/volume
+    obj = json.loads(data)
 
-# convert volume to time and flow to pressure
+# Residual volume is for Flow V Volume
+# pre: a NumPy array (x axis) of Volume(t)
+#      a NumPy array (y-axis) of Flow
+def residualVolume(volume, flow):
+    for (x, y) in zip(volume, flow):
+        if x != 0 and y == 0 :
+            return x
+
+    return "no data found";
+
+def totalLungCapacity(volume):
+    pass
+
+# Testing
+
+volume = np.array([0,1,2,3,4,5])
+flow = np.array([0,1,3,5,6,0])
+
+cock = residualVolume(volume, flow)
+print(cock)
